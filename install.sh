a0f30ed441e49550b193cb98ad1d0d0eb060e641
GhostlyDark <31742919+GhostlyDark@users.noreply.github.com>
2022-11-06 16:35:48 +0100

    install.sh: Improve build process

--- install.sh
+++ install.sh
@@ -1,17 +1,31 @@
 # Install dependencies
-sudo apt install -y build-essential libz-dev libpng-dev libfreetype-dev libsamplerate-dev libspeexdsp-dev liblircclient-dev libboost-dev libboost-filesystem-dev libhidapi-hidraw0 libhidapi-dev libsdl2-dev cmake gcc git make nasm p7zip-full p7zip-rar unzip wget freeglut3-dev
+sudo apt install -y build-essential cmake freeglut3-dev gcc git libboost-dev libboost-filesystem-dev libfreetype-dev libhidapi-dev libhidapi-hidraw0 liblircclient-dev libpng-dev libsamplerate0-dev libsdl2-dev libspeexdsp-dev make nasm p7zip-full p7zip-rar unzip wget zlib1g-dev
 #cargo
 
-# mupen64plus building
-echo "************************************ Creating directories"
+# Variables
+ELECTRON="v21.0.1"
+EMG="EMG/resources/app/m64p/"
+MAKE_INSTALL="PLUGINDIR= SHAREDIR= BINDIR= MANDIR= LIBDIR= APPSDIR= ICONSDIR=icons INCDIR=api LDCONFIG=true COREDIR=./ NEW_DYNAREC=1 SSE=SSE2"
+M64P_COMPONENTS="mupen64plus-core mupen64plus-rom mupen64plus-ui-console mupen64plus-audio-sdl mupen64plus-input-sdl mupen64plus-rsp-hle rsp mupen64plus-video-rice mupen64plus-video-glide64mk2"
+
 mkdir -p source
 
-# mupen64plus components
-if [ -z "$M64P_COMPONENTS" ]; then
-	M64P_COMPONENTS="mupen64plus-core mupen64plus-rom mupen64plus-ui-console mupen64plus-audio-sdl mupen64plus-input-sdl mupen64plus-rsp-hle rsp mupen64plus-video-rice mupen64plus-video-glide64mk2"
-fi
+# Download source code
+git clone https://github.com/GhostlyDark/EMG.git EMG
+git clone https://github.com/GhostlyDark/sdl-jstest.git source/sdl-jstest
+git clone https://github.com/GhostlyDark/SDL_GameControllerDB source/SDL_GameControllerDB
+#git clone https://github.com/GhostlyDark/mupen64plus-input-gca.git source/mupen64plus-input-gca
+git clone https://github.com/GhostlyDark/mupen64plus-input-raphnetraw.git source/mupen64plus-input-raphnetraw
+git clone https://github.com/GhostlyDark/GLideN64.git source/GLideN64
+git clone https://github.com/GhostlyDark/angrylion-rdp-plus.git source/angrylion-rdp-plus
+#git clone https://github.com/GhostlyDark/parallel-rdp-standalone.git source/parallel-rdp-standalone
+git clone https://github.com/GhostlyDark/parallel-rsp.git source/parallel-rsp
 
-# mupen64plus downloading
+# Download Electron
+[ ! -f electron-${ELECTRON}-linux-x64.zip ] && wget https://github.com/electron/electron/releases/download/${ELECTRON}/electron-${ELECTRON}-linux-x64.zip
+unzip electron-${ELECTRON}-linux-x64.zip -o -d EMG -x LICENSE
+
+# Download mupen64plus source code
 for component in ${M64P_COMPONENTS}; do
 	if [ "${component}" = "mupen64plus-core" ]; then
 		component_type="library"
@@ -23,27 +37,17 @@ for component in ${M64P_COMPONENTS}; do
 		component_type="plugin"
 	fi
 
-	echo "************************************ Downloading ${component} ${component_type}"
+	echo "*** Downloading ${component} ${component_type} ***"
 	git clone https://github.com/GhostlyDark/${component}.git source/${component} $@
 done
 
-if [ -z "$MAKE" ]; then
-	MAKE=make
-fi
-
-mkdir -p ./m64p-build/
-
-# mupen64plus build flags
-MAKE_INSTALL="PLUGINDIR= SHAREDIR= BINDIR= MANDIR= LIBDIR= APPSDIR= ICONSDIR=icons INCDIR=api LDCONFIG=true COREDIR=./ NEW_DYNAREC=1 SSE=SSE2"
-
-# mupen64plus compiling
+# Build mupen64plus components
 for component in ${M64P_COMPONENTS}; do
 	if [ "${component}" = "mupen64plus-core" ]; then
 		component_type="library"
 	elif  [ "${component}" = "mupen64plus-rom" ]; then
-		echo "************************************ Building test ROM"
-		mkdir -p ./m64p-build/
-		cp source/mupen64plus-rom/m64p_test_rom.v64 ./m64p-build/
+		echo "*** Building test ROM ***"
+		cp source/mupen64plus-rom/m64p_test_rom.v64 ${EMG}
 		continue
 	elif  [ "${component}" = "mupen64plus-ui-console" ]; then
 		component_type="front-end"
@@ -51,23 +55,12 @@ for component in ${M64P_COMPONENTS}; do
 		component_type="plugin"
 	fi
 
-	echo "************************************ Building ${component} ${component_type}"
-	"$MAKE" -j4 -C source/${component}/projects/unix clean $@
-	"$MAKE" -j4 -C source/${component}/projects/unix all $@ ${MAKE_INSTALL}
-	"$MAKE" -j4 -C source/${component}/projects/unix install $@ ${MAKE_INSTALL} DESTDIR="$(pwd)/m64p-build/"
+	echo "*** Building ${component} ${component_type} ***"
+	make -j4 -C source/${component}/projects/unix clean $@
+	make -j4 -C source/${component}/projects/unix all $@ ${MAKE_INSTALL}
+	make -j4 -C source/${component}/projects/unix install $@ ${MAKE_INSTALL} DESTDIR="$(pwd)/${EMG}"
 
 done
-
-# Download additional components
-git clone https://github.com/GhostlyDark/EMG.git EMG
-git clone https://github.com/GhostlyDark/sdl-jstest.git source/sdl-jstest
-git clone https://github.com/GhostlyDark/SDL_GameControllerDB source/SDL_GameControllerDB
-#git clone https://github.com/GhostlyDark/mupen64plus-input-gca.git source/mupen64plus-input-gca
-git clone https://github.com/GhostlyDark/mupen64plus-input-raphnetraw.git source/mupen64plus-input-raphnetraw
-git clone https://github.com/GhostlyDark/GLideN64.git source/GLideN64
-git clone https://github.com/GhostlyDark/angrylion-rdp-plus.git source/angrylion-rdp-plus
-#git clone https://github.com/GhostlyDark/parallel-rdp-standalone.git source/parallel-rdp-standalone
-git clone https://github.com/GhostlyDark/parallel-rsp.git source/parallel-rsp
 
 # Build sdl2-jstest
 cd source/sdl-jstest
@@ -117,21 +110,16 @@ cmake ..
 make -j4
 cd ../../../
 
-# Download Electron
-wget https://github.com/electron/electron/releases/download/v21.0.1/electron-v21.0.1-linux-x64.zip
-unzip electron-v21.0.1-linux-x64.zip -d EMG -x LICENSE
-
 # Prepare files
-cp -r m64p-build/* EMG/resources/app/m64p/
-cp -r source/sdl-jstest/build/sdl2-jstest EMG/resources/app/m64p/
-cp -r source/SDL_GameControllerDB/gamecontrollerdb.txt EMG/resources/app/m64p/
-#cp -r source/mupen64plus-input-gca/target/release/mupen64plus_input_gca.so EMG/resources/app/m64p/
-cp -r source/mupen64plus-input-raphnetraw/projects/unix/mupen64plus-input-raphnetraw.so EMG/resources/app/m64p/
-cp -r source/GLideN64/src/build/plugin/Release/mupen64plus-video-GLideN64.so EMG/resources/app/m64p/
-cp -r source/GLideN64/ini/GLideN64.custom.ini EMG/resources/app/m64p/
-cp -r source/angrylion-rdp-plus/build/mupen64plus-video-angrylion-plus.so EMG/resources/app/m64p/
-#cp -r source/parallel-rdp-standalone/build/mupen64plus-video-parallel.so EMG/resources/app/m64p/
-cp -r source/parallel-rsp/build/mupen64plus-rsp-parallel.so EMG/resources/app/m64p/
+cp source/sdl-jstest/build/sdl2-jstest ${EMG}
+cp source/SDL_GameControllerDB/gamecontrollerdb.txt ${EMG}
+#cp source/mupen64plus-input-gca/target/release/mupen64plus_input_gca.so ${EMG}/mupen64plus-input-gca.so
+cp source/mupen64plus-input-raphnetraw/projects/unix/mupen64plus-input-raphnetraw.so ${EMG}
+cp source/GLideN64/src/build/plugin/Release/mupen64plus-video-GLideN64.so ${EMG}
+cp source/GLideN64/ini/GLideN64.custom.ini ${EMG}
+cp source/angrylion-rdp-plus/build/mupen64plus-video-angrylion-plus.so ${EMG}
+#cp source/parallel-rdp-standalone/build/mupen64plus-video-parallel.so ${EMG}
+cp source/parallel-rsp/build/mupen64plus-rsp-parallel.so ${EMG}
 
 cd EMG
 mv electron emg
