de8e3cc2ec3e57293fe5e43e7a44b6c900b05e72
GhostlyDark <31742919+GhostlyDark@users.noreply.github.com>
2022-11-06 02:04:21 +0100

    install.sh: Merge with m64p scripts

--- install.sh
+++ install.sh
@@ -1,15 +1,83 @@
+# Terminate the script if any commands return a non-zero error code
+set -e
+
+# Install dependencies
 sudo apt install -y build-essential libz-dev libpng-dev libfreetype-dev libsamplerate-dev libspeexdsp-dev liblircclient-dev libboost-dev libboost-filesystem-dev libhidapi-hidraw0 libhidapi-dev libsdl2-dev cmake gcc git make nasm p7zip-full p7zip-rar unzip wget
 
+# mupen64plus building
+echo "************************************ Creating directories"
+mkdir source
+
+# mupen64plus components
+if [ -z "$M64P_COMPONENTS" ]; then
+	M64P_COMPONENTS="mupen64plus-core mupen64plus-rom mupen64plus-ui-console mupen64plus-audio-sdl mupen64plus-input-sdl mupen64plus-rsp-hle rsp mupen64plus-video-rice mupen64plus-video-glide64mk2"
+fi
+
+# mupen64plus downloading
+for component in ${M64P_COMPONENTS}; do
+	if [ "${component}" = "mupen64plus-core" ]; then
+		component_type="library"
+	elif  [ "${component}" = "mupen64plus-rom" ]; then
+		component_type=""
+	elif  [ "${component}" = "mupen64plus-ui-console" ]; then
+		component_type="front-end"
+	else
+		component_type="plugin"
+	fi
+
+	echo "************************************ Downloading ${component} ${component_type}"
+	git clone https://github.com/GhostlyDark/${component}.git source/${component} $@
+done
+
+if [ -z "$MAKE" ]; then
+	MAKE=make
+fi
+
+mkdir -p ./m64p-build/
+
+# mupen64plus build flags
+MAKE_INSTALL="PLUGINDIR= SHAREDIR= BINDIR= MANDIR= LIBDIR= APPSDIR= ICONSDIR=icons INCDIR=api LDCONFIG=true NEW_DYNAREC=1 COREDIR=./"
+
+# mupen64plus compiling
+for component in ${M64P_COMPONENTS}; do
+	if [ "${component}" = "mupen64plus-core" ]; then
+		component_type="library"
+	elif  [ "${component}" = "mupen64plus-rom" ]; then
+		echo "************************************ Building test ROM"
+		mkdir -p ./m64p-build/
+		cp source/mupen64plus-rom/m64p_test_rom.v64 ./m64p-build/
+		continue
+	elif  [ "${component}" = "mupen64plus-ui-console" ]; then
+		component_type="front-end"
+	else
+		component_type="plugin"
+	fi
+
+	echo "************************************ Building ${component} ${component_type}"
+	"$MAKE" -C source/${component}/projects/unix clean $@
+	"$MAKE" -C source/${component}/projects/unix all $@ ${MAKE_INSTALL}
+	"$MAKE" -C source/${component}/projects/unix install $@ ${MAKE_INSTALL} DESTDIR="$(pwd)/m64p-build/"
+
+	mkdir -p ./m64p-build/doc
+	for doc in LICENSES README RELEASE; do
+		if [ -e "source/${component}/${doc}" ]; then
+			cp "source/${component}/${doc}" "./m64p-build/doc/${doc}-${component}"
+		fi
+	done
+	for subdoc in gpl-license font-license lgpl-license module-api-versions.txt; do
+		if [ -e "source/${component}/doc/${subdoc}" ]; then
+			cp "source/${component}/doc/${subdoc}" ./m64p-build/doc/
+		fi
+	done
+done
+
+# Downloading additional components
 git clone https://github.com/GhostlyDark/EMG.git EMG
 git clone https://github.com/GhostlyDark/sdl-jstest.git sdl-jstest
 git clone https://github.com/raphnet/mupen64plus-input-raphnetraw.git mupen64plus-input-raphnetraw
 git clone https://github.com/mupen64plus/mupen64plus-core.git mupen64plus-core
 
-cd EMG/assets/scripts
-chmod u+x ./m64p_build.sh
-./m64p_build.sh NEW_DYNAREC=1 COREDIR=./
-cd ../../../
-
+# Build sdl2-jstest
 cd sdl-jstest
 mkdir build
 cd build
@@ -17,14 +85,16 @@ cmake ..
 make
 cd ../../
 
+# Build Raphnetraw Input Plugin
 cd mupen64plus-input-raphnetraw/projects/unix
 make all
 cd ../../../
 
+# Download Electron
 wget https://github.com/electron/electron/releases/download/v21.0.1/electron-v21.0.1-linux-x64.zip
 unzip electron-v21.0.1-linux-x64.zip -d EMG -x LICENSE
 
-cp -r EMG/assets/scripts/m64p-build/* EMG/resources/app/m64p/
+cp -r m64p-build/* EMG/resources/app/m64p/
 cp -r sdl-jstest/build/sdl2-jstest EMG/resources/app/m64p/
 cp -r mupen64plus-input-raphnetraw/projects/unix/mupen64plus-input-raphnetraw.so EMG/resources/app/m64p/
 
