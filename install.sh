3fecf1af9c45ca725c7e93abae90f919ca1fa79c
GhostlyDark <31742919+GhostlyDark@users.noreply.github.com>
2022-11-09 23:11:51 +0100

    install.sh: Remove unzip dependency

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 # Install dependencies
-sudo apt install -y build-essential cmake curl freeglut3-dev gcc git libfreetype-dev libhidapi-dev libhidapi-hidraw0 liblircclient-dev libpng-dev libsamplerate0-dev libsdl2-dev libspeexdsp-dev make nasm p7zip-full p7zip-rar unzip wget zlib1g-dev
+sudo apt install -y build-essential cmake curl freeglut3-dev gcc git libfreetype-dev libhidapi-dev libhidapi-hidraw0 liblircclient-dev libpng-dev libsamplerate0-dev libsdl2-dev libspeexdsp-dev make nasm p7zip-full p7zip-rar wget zlib1g-dev
 
 # Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
@@ -26,7 +26,7 @@ git clone https://github.com/GhostlyDark/parallel-rsp.git source/parallel-rsp
 
 # Download Electron
 [ ! -f electron-${ELECTRON}-linux-x64.zip ] && wget https://github.com/electron/electron/releases/download/${ELECTRON}/electron-${ELECTRON}-linux-x64.zip
-unzip -o electron-${ELECTRON}-linux-x64.zip -d EMG -x LICENSE
+7z x electron-${ELECTRON}-linux-x64.zip -oEMG '-x!LICENSE' -y
 
 # Download mupen64plus source code
 for component in ${M64P_COMPONENTS}; do
