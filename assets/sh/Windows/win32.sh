1ddb193d98d051550efaa1bb38322fd62611d826
GhostlyDark <31742919+GhostlyDark@users.noreply.github.com>
2022-11-14 00:03:10 +0100

    Build: Scripts cleanup

--- assets/sh/Windows/win32.sh
+++ assets/sh/Windows/win32.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-set -ex
+
+# Variables
+electron="v21.2.2"
 script_dir="$(dirname "$0")"
 toplvl_dir="$(realpath "$script_dir")"
 bin_dir="$toplvl_dir/Bin"
@@ -12,17 +14,15 @@ ico_dir="$assets_dir/ico"
 install_dir="$emg_dir/resources/app/m64p"
 threads="${2:-$(nproc)}"
 
-msys2="1"
-generator="MSYS Makefiles"
 
-ELECTRON="v21.2.2"
-EMG="EMG/resources/app/m64p/"
-
+# Create directories
 pushd "$toplvl_dir"
 
 mkdir -p "$build_dir"
 mkdir -p "$bin_dir"
 
+
+# Download binary files
 pushd "$bin_dir"
 
 if [ ! -d "7z-binary" ] ; then
@@ -32,22 +32,22 @@ fi
 cp 7z-binary/7z.dll $install_dir
 cp 7z-binary/7z.exe $install_dir
 
-[ ! -f electron-${ELECTRON}-win32-x64.zip ] && wget https://github.com/electron/electron/releases/download/${ELECTRON}/electron-${ELECTRON}-win32-x64.zip
-7z x electron-${ELECTRON}-win32-x64.zip -o../EMG '-x!LICENSE' -y
+[ ! -f electron-$electron-win32-x64.zip ] && wget https://github.com/electron/electron/releases/download/$electron/electron-$electron-win32-x64.zip
+7z x electron-$electron-win32-x64.zip -o../EMG '-x!LICENSE' -y
 
 [ ! -f rcedit-x64.exe ] && wget https://github.com/electron/rcedit/releases/download/v1.1.1/rcedit-x64.exe
 
-pushd "$emg_dir"
 
-mv electron.exe EMG.exe
-
+# Build
 pushd "$build_dir"
 
-cmake -S "$toplvl_dir" -B "$build_dir" -DCMAKE_BUILD_TYPE="Release" -DPORTABLE_INSTALL=ON -G "$generator"
+cmake -S "$toplvl_dir" -B "$build_dir" -DCMAKE_BUILD_TYPE="Release" -DPORTABLE_INSTALL=ON -G "MSYS Makefiles"
 
 make install DESTDIR="$toplvl_dir" -j$threads
 make bundle_dependencies
 
+
+# Download additional sources
 pushd "$source_dir"
 
 if [ ! -d "mupen64plus-rom" ] ; then
@@ -61,15 +61,26 @@ fi
 cp mupen64plus-rom/m64p_test_rom.v64 $install_dir
 cp SDL_GameControllerDB/gamecontrollerdb.txt $install_dir
 
-pushd "$toplvl_dir"
 
-cmd //c $bin_dir/rcedit-x64 $install_dir/mupen64plus.exe --set-icon $ico_dir/mupen64plus.ico
-cmd //c $bin_dir/rcedit-x64 $emg_dir/EMG.exe --set-icon $ico_dir/emg.ico --set-version-string LegalCopyright "(C) 2022 EvilGames.eu" --set-version-string  OriginalFilename "electron.exe" --set-version-string FileDescription "EMG" --set-version-string ProductName "EMG" --set-version-string CompanyName "EvilGames.eu"
+# Rename files
+pushd "$emg_dir"
+
+mv electron.exe EMG.exe
 
 pushd "$install_dir"
 
 mv mupen64plus_input_gca.dll mupen64plus-input-gca.dll
 
+
+# rcedit
+pushd "$toplvl_dir"
+
+cmd //c $bin_dir/rcedit-x64 $install_dir/mupen64plus.exe --set-icon $ico_dir/mupen64plus.ico
+cmd //c $bin_dir/rcedit-x64 $emg_dir/EMG.exe --set-icon $ico_dir/emg.ico --set-version-string LegalCopyright "(C) 2022 EvilGames.eu" --set-version-string  OriginalFilename "electron.exe" --set-version-string FileDescription "EMG" --set-version-string ProductName "EMG" --set-version-string CompanyName "EvilGames.eu"
+
+
+# Strip binaries
 for f in $install_dir/*.dll; do strip -s $f; done
+
 
 popd
--- assets/sh/linux.sh
+++ assets/sh/linux.sh
@@ -1,9 +1,11 @@
 # Install dependencies
 sudo apt install -y build-essential cmake curl freeglut3-dev gcc git libfreetype-dev libhidapi-dev libhidapi-hidraw0 libpng-dev libsdl2-dev make nasm p7zip-full p7zip-rar wget zlib1g-dev
 
+
 # Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
+
 
 # Variables
 ELECTRON="v21.2.2"
@@ -12,7 +14,10 @@ MAKE_INSTALL="PLUGINDIR= SHAREDIR= BINDIR= MANDIR= LIBDIR= APPSDIR= ICONSDIR=ico
 M64P_COMPONENTS="mupen64plus-core mupen64plus-ui-console mupen64plus-audio-sdl mupen64plus-input-sdl mupen64plus-input-raphnetraw mupen64plus-rsp-hle rsp"
 SOURCE="mupen64plus-rom sdl-jstest SDL_GameControllerDB mupen64plus-input-gca GLideN64 angrylion-rdp-plus parallel-rdp-standalone parallel-rsp"
 
+
+# Create source directory
 mkdir -p source
+
 
 # Download source code
 git clone --depth 1 https://github.com/GhostlyDark/EMG.git EMG
@@ -21,15 +26,18 @@ for component in ${M64P_COMPONENTS} ${SOURCE}; do
 	git clone --depth 1 https://github.com/GhostlyDark/${component}.git source/${component} $@
 done
 
+
 # Download and extract Electron
 [ ! -f electron-${ELECTRON}-linux-x64.zip ] && wget https://github.com/electron/electron/releases/download/${ELECTRON}/electron-${ELECTRON}-linux-x64.zip
 7z x electron-${ELECTRON}-linux-x64.zip -oEMG '-x!LICENSE' -y
+
 
 # Build mupen64plus components
 for component in ${M64P_COMPONENTS}; do
 	make -j4 -C source/${component}/projects/unix all $@ ${MAKE_INSTALL}
 	make -j4 -C source/${component}/projects/unix install $@ ${MAKE_INSTALL} DESTDIR="$(pwd)/${EMG}"
 done
+
 
 # Build sdl2-jstest
 cd source/sdl-jstest
@@ -39,10 +47,12 @@ cmake ..
 make -j4
 cd ../../../
 
+
 # Build mupen64plus-input-gca
 cd source/mupen64plus-input-gca
 cargo build --release --features "m64p_compat"
 cd ../../
+
 
 #Build mupen64plus-video-GLideN64
 cd source/GLideN64/src
@@ -52,6 +62,7 @@ cmake .. -DMUPENPLUSAPI=On -DUSE_SYSTEM_LIBS=ON
 make -j4
 cd ../../../../
 
+
 # Build mupen64plus-video-angrylion-plus
 cd source/angrylion-rdp-plus
 mkdir -p build
@@ -59,6 +70,7 @@ cd build
 cmake ..
 make -j4
 cd ../../../
+
 
 # Build mupen64plus-video-parallel
 cd source/parallel-rdp-standalone
@@ -68,6 +80,7 @@ cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j4
 cd ../../../
 
+
 # Build mupen64plus-rsp-parallel
 cd source/parallel-rsp
 mkdir -p build
@@ -75,6 +88,7 @@ cd build
 cmake ..
 make -j4
 cd ../../../
+
 
 # Prepare files
 cp source/mupen64plus-rom/m64p_test_rom.v64 ${EMG}
@@ -87,9 +101,12 @@ cp source/angrylion-rdp-plus/build/mupen64plus-video-angrylion-plus.so ${EMG}
 cp source/parallel-rdp-standalone/build/mupen64plus-video-parallel.so ${EMG}
 cp source/parallel-rsp/build/mupen64plus-rsp-parallel.so ${EMG}
 
+
 # Strip binaries
 for f in ${EMG}/*.so; do strip -s $f; done
 
+
+# Rename and run executable
 cd EMG
 mv electron emg
 ./emg
