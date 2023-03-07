13362b5a66bc49332b5303c1809742cc65553e61
GhostlyDark <31742919+GhostlyDark@users.noreply.github.com>
2023-03-07 20:58:33 +0100

    EMG: Decouple core from R4300 emulator

--- resources/app/emg.js
+++ resources/app/emg.js
@@ -1189,11 +1189,9 @@ buttonType1 = '',buttonType1B = '',buttonType2 = '',buttonType2B = '',buttonType
 
 gcaSettings = 'control_stick_deadzone = ' +  id('control_stick_deadzone').value + '\n' + 'control_stick_sensitivity = ' + id('control_stick_sensitivity').value + '\n' + 'c_stick_deadzone = ' + id('c_stick_deadzone').value + '\n' + 'trigger_threshold = ' + id('trigger_threshold').value + '\n\n' + '[controller_mapping]' + '\n' + 'a = ' + id('a').value + '\n' + 'b = ' + id('b').value + '\n' + 'x = ' + id('x').value + '\n' + 'y = ' + id('y').value + '\n' + 'start = ' + id('start').value + '\n' + 'z = ' + id('z').value + '\n' + 'l = ' + id('l').value + '\n' + 'r = ' + id('r').value + '\n' + 'd_pad_left = ' + id('d_pad_left').value + '\n' + 'd_pad_right = ' + id('d_pad_right').value + '\n' + 'd_pad_down = ' + id('d_pad_down').value + '\n' + 'd_pad_up = ' + id('d_pad_up').value + '\n' + 'c_stick_left = ' + id('c_stick_left').value + '\n' + 'c_stick_right = ' + id('c_stick_right').value + '\n' + 'c_stick_down = ' + id('c_stick_down').value + '\n' + 'c_stick_up = ' + id('c_stick_up').value;
 
-if(id('emumode').value === '3'){emumode = 'Core[R4300Emulator]=2';corelib = 'core/mupen64old'} /* R4300 emulator */
-if(isLinux){
-if(id('emumode').value === '3'){corelib = 'core/mupen64old.so'}
-else{corelib = 'core/mupen64plus.so'};
-RspFallback += '.so'} /* Linux RSP fallback */
+corelib = 'core/' + id('coreVersion').value;
+
+if(isLinux){corelib += '.so';RspFallback += '.so'} /* Linux core file and RSP fallback extension */
 
 if(id('nospeedlimit').checked){audio = 'dummy';vsync = 'Video-General[VerticalSync]=false';ParallelVSync = 'Video-Parallel[Vsync]=false'} /* force muted audio and disabled V-Sync */
 
