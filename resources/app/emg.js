728075586183a2edb655523fa1157ffb1af91c29
GhostlyDark <31742919+GhostlyDark@users.noreply.github.com>
2023-04-18 18:38:07 +0200

    emg.js: Use correct file extensions

--- resources/app/emg.js
+++ resources/app/emg.js
@@ -1191,18 +1191,18 @@ buttonType1 = '',buttonType1B = '',buttonType2 = '',buttonType2B = '',buttonType
 gcaSettings = 'control_stick_deadzone = ' +  id('control_stick_deadzone').value + '\n' + 'control_stick_sensitivity = ' + id('control_stick_sensitivity').value + '\n' + 'c_stick_deadzone = ' + id('c_stick_deadzone').value + '\n' + 'trigger_threshold = ' + id('trigger_threshold').value + '\n\n' + '[controller_mapping]' + '\n' + 'a = ' + id('a').value + '\n' + 'b = ' + id('b').value + '\n' + 'x = ' + id('x').value + '\n' + 'y = ' + id('y').value + '\n' + 'start = ' + id('start').value + '\n' + 'z = ' + id('z').value + '\n' + 'l = ' + id('l').value + '\n' + 'r = ' + id('r').value + '\n' + 'd_pad_left = ' + id('d_pad_left').value + '\n' + 'd_pad_right = ' + id('d_pad_right').value + '\n' + 'd_pad_down = ' + id('d_pad_down').value + '\n' + 'd_pad_up = ' + id('d_pad_up').value + '\n' + 'c_stick_left = ' + id('c_stick_left').value + '\n' + 'c_stick_right = ' + id('c_stick_right').value + '\n' + 'c_stick_down = ' + id('c_stick_down').value + '\n' + 'c_stick_up = ' + id('c_stick_up').value;
 
 corelib = 'core/' + id('coreVersion').value;
-
-if(isLinux){corelib += '.so';RspFallback += '.so'} /* Linux core file and RSP fallback extension */
-
-if(id('nospeedlimit').checked){audio = 'dummy';vsync = 'Video-General[VerticalSync]=false';ParallelVSync = 'Video-Parallel[Vsync]=false'} /* force muted audio and disabled V-Sync */
-
-if(id('gliden64Version').value != '_'){gfx += id('gliden64Version').value} /* GLideN64 version */
+if(id('gliden64Version').value != '_'){gfx += id('gliden64Version').value}
 
 if(gfx.includes('angrylion') || gfx.includes('parallel')){cxd4GFX = 'rsp-cxd4[DisplayListToGraphicsPlugin]=false'} /* prevent crashes caused by wrong RSP settings */
 else if(gfx.includes('rice')){cxd4GFX = 'rsp-cxd4[DisplayListToGraphicsPlugin]=true'}
 if(gfx.includes('angrylion') && rsp.includes('rsp-hle')){rsp = 'mupen64plus-rsp-cxd4'}
 else if(gfx.includes('parallel') && rsp.includes('rsp-hle')){rsp = 'mupen64plus-rsp-parallel'}
 else if(gfx.includes('rice') && rsp.includes('rsp-parallel')){rsp = 'mupen64plus-rsp-hle'}
+
+if(isLinux){corelib += '.so'; gfx += '.so'; audio += '.so'; input += '.so'; rsp += '.so'; RspFallback += '.so'}
+else{corelib += '.dll'; gfx += '.dll'; audio += '.dll'; input += '.dll', rsp += '.dll'; RspFallback += '.dll'}
+
+if(id('nospeedlimit').checked){audio = 'dummy';vsync = 'Video-General[VerticalSync]=false';ParallelVSync = 'Video-Parallel[Vsync]=false'} /* force muted audio and disabled V-Sync */
 
 
 
