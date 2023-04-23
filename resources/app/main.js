824374395ae2003c24281e4cd6c62006af8e698a
GhostlyDark <31742919+GhostlyDark@users.noreply.github.com>
2023-04-23 15:04:22 +0200

    main.js: Resize win icon
    Fixes window icon not loading on Linux

--- resources/app/main.js
+++ resources/app/main.js
@@ -20,7 +20,6 @@ stdio = ['ignore', 'pipe', 'ignore'],
 emuOptions = {cwd: cwd, detached: true, stdio: stdio},
 cheatOptions = {cwd: cwd, stdio: stdio, windowsHide: true},
 jsOptions = {cwd: cwd, stdio: stdio, timeout: 5000, windowsHide: true},
-scale = {width:28},
 load = path(dir, 'emg.htm'),
 name = ' ' + app.name + ' v' + app.getVersion(),
 preferences = {preload:path(dir, 'preload.js')},
@@ -101,7 +100,7 @@ win = new BrowserWindow(mainWindow)
 win.loadFile(load)
 win.once('ready-to-show', () => {win.maximize();win.show()})
 win.on('page-title-updated', (e) => {e.preventDefault()})
-if(isLinux){win.setIcon(path(dir, 'img', 'emg.png'))}
+if(isLinux)win.setIcon(nativeImage.createFromPath(path(dir, 'img', 'emg.png')).resize({width:48}))
 win.webContents.setWindowOpenHandler((details) => {return {action:'deny'}})
 
 win.webContents.on('will-navigate', (e, nav) => {const parsed = new url(nav)
@@ -114,22 +113,22 @@ if(whitelist.test(details.url)){callback({cancel:false})}
 else{callback({cancel:true})}})
 
 Menu.setApplicationMenu(Menu.buildFromTemplate([
-	{label: 'App', submenu: [{icon: nativeImage.createFromPath(path(dir, 'img', 'quit.png')).resize(scale), label: 'Quit ' + app.name, click () {win.close()}}]},
+	{label: 'App', submenu: [{icon: nativeImage.createFromPath(path(dir, 'img', 'quit.png')).resize({width:28}), label: 'Quit ' + app.name, click () {win.close()}}]},
 	{label: 'Window', submenu: [
-		{icon: nativeImage.createFromPath(path(dir, 'img', 'refresh.png')).resize(scale), label: 'Reload window', accelerator: 'CmdOrCtrl+R', role: 'reload'},
-		{icon: nativeImage.createFromPath(path(dir, 'img', 'inspector.png')).resize(scale), label: 'Developer tools', accelerator: 'CmdOrCtrl+I', role: 'toggleDevTools'},
+		{icon: nativeImage.createFromPath(path(dir, 'img', 'refresh.png')).resize({width:28}), label: 'Reload window', accelerator: 'CmdOrCtrl+R', role: 'reload'},
+		{icon: nativeImage.createFromPath(path(dir, 'img', 'inspector.png')).resize({width:28}), label: 'Developer tools', accelerator: 'CmdOrCtrl+I', role: 'toggleDevTools'},
 		{type: 'separator'},
-		{icon: nativeImage.createFromPath(path(dir, 'img', 'zoom-in.png')).resize(scale), label: 'Increase zoom', accelerator: 'CmdOrCtrl+numadd', role: 'zoomin'},
-		{icon: nativeImage.createFromPath(path(dir, 'img', 'zoom-out.png')).resize(scale), label: 'Decrease zoom', accelerator: 'CmdOrCtrl+numsub', role: 'zoomout'},
-		{icon: nativeImage.createFromPath(path(dir, 'img', 'zoom-reset.png')).resize(scale), label: 'Reset zoom', accelerator: 'CmdOrCtrl+num0', role: 'resetzoom'},
+		{icon: nativeImage.createFromPath(path(dir, 'img', 'zoom-in.png')).resize({width:28}), label: 'Increase zoom', accelerator: 'CmdOrCtrl+numadd', role: 'zoomin'},
+		{icon: nativeImage.createFromPath(path(dir, 'img', 'zoom-out.png')).resize({width:28}), label: 'Decrease zoom', accelerator: 'CmdOrCtrl+numsub', role: 'zoomout'},
+		{icon: nativeImage.createFromPath(path(dir, 'img', 'zoom-reset.png')).resize({width:28}), label: 'Reset zoom', accelerator: 'CmdOrCtrl+num0', role: 'resetzoom'},
 		]},
 	{label: 'Functions', submenu: [
-		{icon: nativeImage.createFromPath(path(dir, 'img', 'delete.png')).resize(scale), label: 'Reset settings', click () {const choice = dialog.showMessageBoxSync(win,deleteDialog);if(choice !== 1){session.defaultSession.clearStorageData();session.defaultSession.clearCache()}}},
-		{icon: nativeImage.createFromPath(path(dir, 'img', 'emg.png')).resize(scale), label: 'Show EMG data', click () {shell.openPath(emg)}},
-		{icon: nativeImage.createFromPath(path(dir, 'img', 'mupen64plus.png')).resize(scale), label: 'Show m64p data', click () {shell.openPath(m64pConfig);if(isLinux){shell.openPath(m64pCache);shell.openPath(m64pShare)}}},
+		{icon: nativeImage.createFromPath(path(dir, 'img', 'delete.png')).resize({width:28}), label: 'Reset settings', click () {const choice = dialog.showMessageBoxSync(win,deleteDialog);if(choice !== 1){session.defaultSession.clearStorageData();session.defaultSession.clearCache()}}},
+		{icon: nativeImage.createFromPath(path(dir, 'img', 'emg.png')).resize({width:28}), label: 'Show EMG data', click () {shell.openPath(emg)}},
+		{icon: nativeImage.createFromPath(path(dir, 'img', 'mupen64plus.png')).resize({width:28}), label: 'Show m64p data', click () {shell.openPath(m64pConfig);if(isLinux){shell.openPath(m64pCache);shell.openPath(m64pShare)}}},
 		{type: 'separator'},
-		{icon: nativeImage.createFromPath(path(dir, 'img', 'github.png')).resize(scale), label: 'Visit GitHub repo', click () {shell.openExternal('https://github.com/GhostlyDark/EMG')}},
-		{icon: nativeImage.createFromPath(path(dir, 'img', 'icon-ghostly-nx.png')).resize(scale), label: 'Visit website', click () {shell.openExternal('https://evilgames.eu/')}}
+		{icon: nativeImage.createFromPath(path(dir, 'img', 'github.png')).resize({width:28}), label: 'Visit GitHub repo', click () {shell.openExternal('https://github.com/GhostlyDark/EMG')}},
+		{icon: nativeImage.createFromPath(path(dir, 'img', 'icon-ghostly-nx.png')).resize({width:28}), label: 'Visit website', click () {shell.openExternal('https://evilgames.eu/')}}
 		]}
 ]))
 
