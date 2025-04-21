48401a5262534dae32774165f867c2a1e1fcc9a9
GhostlyDark <31742919+GhostlyDark@users.noreply.github.com>
2025-04-21 16:54:02 +0200

    Update mupen64plus-video-parallel

--- source/mupen64plus-video-parallel/parallel_imp.cpp
+++ source/mupen64plus-video-parallel/parallel_imp.cpp
@@ -254,7 +254,8 @@ void vk_rasterize()
 	auto &device = wsi->get_device();
 	render_frame(device);
 	(*render_callback)(1);
-	wsi->end_frame();
+	if (wsi)
+		wsi->end_frame();
 	wsi->begin_frame();
 }
 
@@ -287,7 +288,8 @@ void vk_read_screen(unsigned char* dest)
 		image_buffer -= window_width * 4;
 		for (int j = 0; j < window_width; ++j)
 		{
-			switch (image->get_format()) {
+			auto image_format = image->get_format();
+			switch (image_format) {
 				case VK_FORMAT_B8G8R8A8_UNORM:
 					dest[0] = image_buffer[2];
 					dest[1] = image_buffer[1];
@@ -303,7 +305,24 @@ void vk_read_screen(unsigned char* dest)
 					dest[1] = image_buffer[2];
 					dest[2] = image_buffer[1];
 					break;
+				case VK_FORMAT_A2B10G10R10_UNORM_PACK32:
+				{
+					uint32_t pixel = *(uint32_t*) image_buffer;
+					dest[0] = pixel >> 2;
+					dest[1] = pixel >> 12;
+					dest[2] = pixel >> 22;
+					break;
+				}
+				case VK_FORMAT_A2R10G10B10_UNORM_PACK32:
+				{
+					uint32_t pixel = *(uint32_t*) image_buffer;
+					dest[0] = pixel >> 22;
+					dest[1] = pixel >> 12;
+					dest[2] = pixel >> 2;
+					break;
+				}
 				default:
+					LOGE("Encountered unknown image format in vk_read_screen: %d\n", image_format);
 					wsi->get_device().unmap_host_buffer(*buffer, Vulkan::MEMORY_ACCESS_READ_BIT);
 					return;
 			}
@@ -475,9 +494,12 @@ bool vk_init()
 	wsi_platform.reset(new MupenWSIPlatform);
 	wsi->set_platform(wsi_platform.get());
 	wsi->set_present_mode(window_vsync ? Vulkan::PresentMode::SyncToVBlank : Vulkan::PresentMode::UnlockedMaybeTear);
-	wsi->set_backbuffer_srgb(false);
+	wsi->set_backbuffer_format(Vulkan::BackbufferFormat::UNORM);
 	if (!wsi->init_simple(1, handles))
+	{
+		vk_destroy();
 		return false;
+	}
 
 	uintptr_t aligned_rdram = reinterpret_cast<uintptr_t>(gfx.RDRAM);
 	uintptr_t offset = 0;
@@ -489,6 +511,7 @@ bool vk_init()
 
 		if (offset)
 		{
+			vk_destroy();
 			return false;
 		}
 		aligned_rdram -= offset;
@@ -521,7 +544,7 @@ bool vk_init()
 											 offset, rdram_size, rdram_size / 2, flags));
 	if (!processor->device_is_supported())
 	{
-		processor.reset();
+		vk_destroy();
 		return false;
 	}
 
