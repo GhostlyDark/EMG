3038f83c76b4056a33b5a926a7363faf1847702b
GhostlyDark <31742919+GhostlyDark@users.noreply.github.com>
2026-07-26 12:02:09 +0200

    Update mupen64plus-video-GLideN64

--- source/mupen64plus-video-GLideN64/src/GLideNUI/ConfigDialog.cpp
+++ source/mupen64plus-video-GLideN64/src/GLideNUI/ConfigDialog.cpp
@@ -220,6 +220,7 @@ void ConfigDialog::_init(bool reInit, bool blockCustomSettings)
 	ui->anisotropicSlider->setValue(anisotropy);
 	ui->vSyncCheckBox->setChecked(config.video.verticalSync != 0);
 	ui->threadedVideoCheckBox->setChecked(config.video.threadedVideo != 0);
+	ui->asyncShadersCheckBox->setChecked(config.video.asyncShaderCompilation != 0);
 
 	switch (config.texture.bilinearMode) {
 	case BILINEAR_3POINT:
@@ -599,6 +600,7 @@ void ConfigDialog::accept(bool justSave) {
 
 	config.video.verticalSync = ui->vSyncCheckBox->isChecked() ? 1 : 0;
 	config.video.threadedVideo = ui->threadedVideoCheckBox->isChecked() ? 1 : 0;
+	config.video.asyncShaderCompilation = ui->asyncShadersCheckBox->isChecked() ? 1 : 0;
 
 	// Emulation settings
 	config.generalEmulation.enableLOD = ui->emulateLodCheckBox->isChecked() ? 1 : 0;
